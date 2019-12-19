9df8746fc34bb4415ee852a71ac0c41e7732ee76
David Van Fleet <dvfleet413@gmail.com>
2019-12-19 01:49:19 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -11,5 +11,6 @@ end
 def print_first_directors_movie_titles
   row_index = 0
   while row_index < directors_database.length do
-    
+    puts "Row #{row_index} has #{directors_database[row_index]} columns"
+  end
 end
