c61fad907c7551821c748eb39456de97dabfe2ee
David Van Fleet <dvfleet413@gmail.com>
2019-12-19 02:02:19 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -9,11 +9,11 @@ def pretty_print_nds(nds)
 end
 
 def print_first_directors_movie_titles
-  row_index = 0
-  while row_index < directors_database.length do
-    puts "Row #{row_index} has #{directors_database[row_index]} columns"
-    row_index += 1
-  end
+ # row_index = 0
+ # while row_index < directors_database.length do
+ #   puts "Row #{row_index} has #{directors_database[row_index]} columns"
+ #   row_index += 1
+ # end
   
   element_index = 0
   while element_index < directors_database[0][:movies].length do
